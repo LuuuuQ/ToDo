2a1e89e1fb8614b079f635043b4225153dfce7fa
LuQ <lp.wroblewski@gmail.com>
2024-02-25 18:58:34 +0100

    todo app final

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse 
 from .models import Task
 # Create your views here.
@@ -7,3 +7,39 @@ def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_complete = True
+    task.save()
+    return redirect('home')
+
+
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_complete = False
+    task.save()
+    return redirect('home')
+
+
+
+def delTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+
+
+def editTask(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task = request.POST['task']
+        get_task.task = task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html', context)
